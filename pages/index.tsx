1944adcb6771e7a9b32622a0a9de127db06434f9
brcelso <celso.sjunior@outlook.com>
2023-04-17 14:51:10 -0300

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function IndexPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="grid items-center gap-6 pt-6 pb-8 md:py-10">
+      <section className="grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
             Beautifully designed components <br className="hidden sm:inline" />
@@ -34,7 +34,7 @@ export default function IndexPage() {
           </p>
         </div>
        
-        <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 md:flex-row">
+        <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0 md:flex-row">
           <Link href="/docs" className={buttonVariants({ size: "lg" })}>
             Get Started
           </Link>
