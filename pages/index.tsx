9f10615be0253bb1b6b1424f7d07d7ad1f1187eb
brcelso <celso.sjunior@outlook.com>
2023-04-17 10:16:06 -0300

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,78 +7,43 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <Layout>
-      <Head>
-        <title>Celso da Silva Junior</title>
-        <meta
-          name="description"
-          content="Next.js template for building apps with Radix UI and Tailwind CSS"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <>
+      <section className="grid items-center gap-6 pt-6 pb-8 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Welcome to New Age !!!! <br className="hidden sm:inline" />
-            built with Radix UI and Tailwind CSS by brcelso.
+          <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-5xl lg:text-6xl lg:leading-[1.1]">
+            Beautifully designed components <br className="hidden sm:inline" />
+            built with Radix UI and Tailwind CSS.
           </h1>
-          <p className="max-w-[700px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
+          <p className="max-w-[750px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
             Accessible and customizable components that you can copy and paste
-            into your apps. Free. Open Source. And Next.js 13 Ready.
+            into your apps. Free. Open Source.{" "}
+            <span className="font-semibold">
+              Use this to build your own component library
+            </span>
+            .
           </p>
         </div>
-        <div className="flex gap-4">
-          <Link
-            href={siteConfig.links.docs}
-            target="_blank"
-            rel="noreferrer"
-            className={buttonVariants({ size: "lg" })}
-          >
-            Documentation
+        
+        <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 md:flex-row">
+          <Link href="/docs" className={buttonVariants({ size: "lg" })}>
+            Get Started
           </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            GitHub
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.vercel}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            Vercel
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.whatsapp}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            WhatsApp
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.instagram}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            Instagram
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.twitter}
-            className={buttonVariants({ variant: "outline", size: "lg" })}
-          >
-            Twitter
-          </Link>
+          
+        </div>
+        <div>
+          <p className="text-sm text-slate-500 dark:text-slate-400">
+            You are looking at an early preview. You can follow the progress on{" "}
+            <Link
+              href={siteConfig.links.twitter}
+              className="font-medium underline underline-offset-4"
+            >
+              Twitter
+            </Link>
+            .
+          </p>
         </div>
       </section>
-    </Layout>
+      
+    </>
   )
 }
